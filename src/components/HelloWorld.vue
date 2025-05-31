ebba21a357362f7416644ce7c0ff7ab1ff83aa23
杨柳岸 <88012771+Yang1aa@users.noreply.github.com>
2025-05-31 23:32:04 +0800

    测试3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: 杨柳岸 88012771+Yang1aa@users.noreply.github.com
+ * @Date: 2025-05-31 23:18:25
+ * @LastEditors: 杨柳岸 88012771+Yang1aa@users.noreply.github.com
+ * @LastEditTime: 2025-05-31 23:31:54
+ * @FilePath: \vite\src\components\HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup>
 import { ref } from 'vue'
 
@@ -15,7 +23,7 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}!!!Yangliuan!!!</button>
     <p>
       Edit 
-      <code>components/HelloWorld.vue</code> to test HMR By Yangliuan, yangliuan
+      <code>components/HelloWorld.vue</code> to test HMR By Yangliuan, yangliuan, yangliuan
     </p>
   </div>
 
